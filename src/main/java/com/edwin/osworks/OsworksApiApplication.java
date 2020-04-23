33e09a052609d0a3fd4aa635a12dbaa019be9e9d
Edwin Lima <edwin.limaa@gmail.com>
2020-04-23 00:01:01 -0300

    Inicio do Projeto.

---
+++ src/main/java/com/edwin/osworks/OsworksApiApplication.java
@@ -0,0 +1,13 @@
+package com.edwin.osworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsworksApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsworksApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/edwin/osworks/domain/repository/ClienteRepository.java
@@ -0,0 +1,11 @@
+package com.edwin.osworks.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.edwin.osworks.domain.model.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long>{
+
+}
